cc2c7c232cf0a127a42a2607d0c47cee2aa78bcc
alec-watts <alecwatts1@gmail.com>
2023-09-14 10:27:40 -0700

    uploadStreams

---
+++ modify.js
@@ -0,0 +1,24 @@
+import db from "./firebase/config.js";
+import { FieldValue } from "firebase-admin/firestore";
+
+const addCreatedAtToAllDocs = async (collectionName) => {
+  const collectionRef = db.collection(collectionName);
+  const snapshot = await collectionRef.get();
+
+  const batch = db.batch();
+
+  snapshot.docs.forEach((doc) => {
+    const docRef = collectionRef.doc(doc.id);
+    batch.update(docRef, {
+      createdAt: FieldValue.serverTimestamp(),
+    });
+  });
+
+  await batch.commit();
+  console.log(`Added createdAt to all documents in ${collectionName}`);
+};
+
+// Usage
+// addCreatedAtToAllDocs("systemPrompts");
+// addCreatedAtToAllDocs("functionCalls");
+addCreatedAtToAllDocs("chats");
